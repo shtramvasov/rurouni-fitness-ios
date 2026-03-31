e81a42546384d885cbade1241b52ff642d30bb4a
anton.shtramvasov <stramvasov@gmail.com>
2026-03-31 13:21:17 +0500

    Initial Commit

---
+++ rurouni-fitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rurouni-fitness
+//
+//  Created by  Антон Штрамвасов on 31.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rurouni-fitness/rurouni_fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  rurouni_fitnessApp.swift
+//  rurouni-fitness
+//
+//  Created by  Антон Штрамвасов on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct rurouni_fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
